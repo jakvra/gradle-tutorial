0127ed169456afaf35e87311574009a0ad7b8761
Jakub Vrana <vrana.jakub@feg.eu>
2023-08-04 19:39:24 +0200

    enable tests in my-webabb project

--- my-webapp/src/test/java/com/example/multimodule/application/DemoApplicationTest.java
+++ my-webapp/src/test/java/com/example/multimodule/application/DemoApplicationTest.java
@@ -1,23 +1,24 @@
 package com.example.multimodule.application;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+import com.example.multimodule.service.MyService;
+import com.google.common.collect.ImmutableSet;
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.example.multimodule.service.MyService;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class DemoApplicationTest {
 
-	@Autowired
-	private MyService myService;
+    @Autowired
+    private MyService myService;
 
-	@Test
-	public void contextLoads() {
-		assertThat(myService.message() + "meh").isNotNull();
-	}
+    @Test
+    public void contextLoads() {
+
+        ImmutableSet.of(1, 2, 3);
+        assertThat(myService.message() + "meh").isNotNull();
+    }
 
 }
